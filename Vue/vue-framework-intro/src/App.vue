cfcebd2d201acfce1db27869141fa7a11f79ecb7
Eric Ocana <eocana.joviat@gmail.com>
2022-07-21 17:21:03 +0200

    Adding routing to the app and scoping styles to components

--- Vue/vue-framework-intro/src/App.vue
+++ Vue/vue-framework-intro/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-   <app-user>
-    
-   </app-user>
+   <!-- <app-user></app-user> not longer necessaty because we adding routing to app-->
+   <router-view></router-view> 
+   <p>This is a test</p>
   </div>
 </template>
 
