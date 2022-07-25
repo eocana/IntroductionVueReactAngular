ca49f216fbce70e7a5261fd31086e9d298f12227
Eric Ocana <eocana.joviat@gmail.com>
2022-07-25 13:19:32 +0200

    Adding react router and routes for the app. Adding links

--- React/react-framework-intro/src/App.js
+++ React/react-framework-intro/src/App.js
@@ -1,11 +1,30 @@
 
 import './App.css';
-import Info from './components/Info'
-
+import Info from './components/Info';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import User from './components/User';
+import Account from './components/Account';
+import Blog from './components/Blog';
 function App() {
   return (
-    <div className="App">
-      <Info/>
+    <div>
+    <BrowserRouter> 
+      <div>
+        <Link to="/">Home</Link> |
+        <Link to="/account">Account</Link> |
+        <Link to="/blog">Blog</Link> |
+      </div>
+      
+      <Routes>
+        <Route exact path="/" element={<User/> }/>
+        <Route path="/account" element={<Account/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+      </Routes>
+        
+      
+       
+
+    </BrowserRouter>
     </div>
   );
 }
---
+++ React/react-framework-intro/src/components/Blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+const Blog = props => {
+   return(
+         <div>
+            <h1>The blog page</h1> 
+        </div>
+   );
+        
+};
+
+export default Blog;
