8384e7d896d7198bba87cf847eac2dc69eefc095
Eric Ocana <eocana.joviat@gmail.com>
2022-07-25 12:01:22 +0200

    Usage of react-app package

---
+++ React/react-framework-intro/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Info from './components/Info'
+
+function App() {
+  return (
+    <div className="App">
+      <Info/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/react-framework-intro/src/components/Info.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Info extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The info</h1>
+                <p>Hello there!</p>
+            </div>
+        )
+    }
+
+}  
+
+export default  Info;
